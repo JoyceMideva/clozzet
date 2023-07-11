5300c5004f79d54b8f23afe0c25fb46dbfc8497e
JoyceMideva <mideva061@gmail.com>
2023-07-11 09:05:39 +0200

    fixed the bugs

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home";
 import Shop from "../pages/Shop";
 import SinglePage from "../pages/SinglePage";
 import Cart from "../pages/Cart";
+import NotFound from "../pages/NotFound";
 function Router() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function Router() {
         <Route path="/shop"  element={<Shop/>}/>
         <Route path="/cart"  element={<Cart/>}/>
         <Route path="/product/:productId"  element={<SinglePage/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer"
+import Nav from "../components/Nav"
+
+function NotFound() {
+    return(
+        <div>
+            <Nav/>
+            <main className="bg-[url(``)]">
+
+
+            </main>
+            <Footer/>
+        </div>
+    )
+}
+export default NotFound
